7b5d3ab44732d230c20cd1930a8b905541344ef9
Marijan Gradecak <gradecam@tcd.ie>
2016-11-04 20:56:34 +0000

    Server now responds to messages correctly, cleaned up code

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,7 @@
 module Main where
 
 import Control.Monad (unless, void )
-import Control.Concurrent (forkFinally, forkIO)
-import Network.Info (getNetworkInterfaces, ipv4)
+import Control.Concurrent (forkFinally)
 import Control.Concurrent.MVar (MVar, takeMVar, putMVar, newMVar)
 import Network.Socket.ByteString (recv, send)
 import qualified Data.ByteString as B
@@ -12,8 +11,9 @@ import Data.List (isInfixOf)
 import System.Exit (exitSuccess)
 import System.Environment (getArgs)
 
-initServer :: String -> String -> IO Net.Socket
-initServer host port = do
+{- Initialise the socket that will be used for the server-}
+initSocket :: String -> String -> IO Net.Socket
+initSocket host port = do
   let hints = Net.defaultHints { Net.addrSocketType = Net.Stream }
   addr:_ <- Net.getAddrInfo (Just hints) (Just host) (Just port)
   print addr
@@ -35,20 +35,11 @@ updateMutex mv op = do
 
 {- Handle an incoming connection, ideally should be run on a new thread to allow multiple
   simultaneous server connections -}
-handle :: Net.Socket -> String -> IO ()
-handle sock inf = do
-  msg <- recv sock 4096 -- readAll sock 1 (return []) -- read the entire message from the socket
+handleIncoming :: Net.Socket -> String -> IO ()
+handleIncoming sock inf = do
+  msg <- recv sock 4096 
   print $ "Message = " ++ unpack msg
-  unless (B.null msg) $ action sock (unpack msg) inf >> handle sock inf
-  print "Im back yo"
-
-{- reads the entire message from the socket, this is used instead of default recv function
-   as recv is not guaranteed to return the entire message -}
-readAll :: Net.Socket -> Int -> IO [B.ByteString] -> IO B.ByteString 
-readAll _ 0 iob = fmap B.concat iob
-readAll s _ m = do
-  msg <- recv s 4096
-  readAll s (B.length msg) $ m >>= (\p -> return(p++[msg]))
+  unless (B.null msg) $ action sock (unpack msg) inf >> handleIncoming sock inf
 
 {- Handles the termination of connection handling threads -}
 endThread :: Net.Socket -> MVar Int -> IO ()
@@ -58,27 +49,27 @@ endThread s m = Net.close s >> updateMutex m (+1)
  Main logic of the Server
  @params:
     Socket on which the server should accept connections
-    Number of connections it should procees at a time
+    Number of connections it should process at a time
     list of details regarding the server -}
 runServer :: Net.Socket -> Int -> String-> IO ()
 runServer sock n inf = do
   Net.listen sock 3                    -- allow maximum of 3 queued connections
-  threads <- newMVar n
-  loop sock threads inf
-  where loop s mu i =  do
+  threads <- newMVar n                 -- create a new mutex variable for counting amount of active threads
+  loop threads
+  where loop mu =  do
           (conn,_) <- Net.accept sock  -- accept incoming connection
           x <- takeMVar mu
           putMVar mu x
           if x == 0                    -- if no free threads available
-            then Net.close conn >> loop s mu i
+            then Net.close conn >> loop mu
             else do
             updateMutex mu (\z -> z-1) -- reduce available thread count
-            _ <- forkFinally (handle conn inf) (\_ -> endThread conn mu )
-            loop s mu i
+            _ <- forkFinally (handleIncoming conn inf) (\_ -> endThread conn mu ) -- handle incoming connection on a new thread
+                                                                                  -- when the thread terminates call 'endThread'
+            loop mu
 
 main :: IO ()
 main = do
   [host, port, n] <- getArgs
-  ns <- getNetworkInterfaces
-  sock <- initServer host port -- intialise the server socket
-  runServer sock  (read n :: Int) $ "IP:"++show (ipv4 $ head ns)++"\nPort:"++port++"\nSudentID:13319506\n"
+  sock <- initSocket host port -- intialise the server socket
+  runServer sock  (read n :: Int) $ "IP:10.62.0.104\nPort:"++port++"\nStudentID:13319506\n"
