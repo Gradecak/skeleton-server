7ced4d3f6bae4f3d1d7adcc3178dedb748dd82da
Marijan Gradecak <gradecam@tcd.ie>
2016-11-04 17:21:34 +0000

    made it do stuff

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
-import Control.Monad (unless, void)
+import Control.Monad (unless, void )
+import Control.Concurrent (forkFinally, forkIO)
+import Control.Concurrent.MVar (MVar, takeMVar, putMVar, newMVar)
 import Network.Socket.ByteString (recv, send)
 import qualified Data.ByteString as B
 import Data.ByteString.Char8 (pack, unpack)
@@ -18,31 +20,64 @@ initServer host port = do
   Net.bind sock (Net.addrAddress addr)
   return sock
 
-getDetails :: String -> [String] -> String
-getDetails msg inf = msg ++ "IP:" ++ head inf ++ "\nPort:" ++ inf !! 1 ++ "\nStudentID:" ++ last inf ++ "\n"
+{- A set of actions that the server will perform on recieveing keyword string -}
+action :: Net.Socket -> String -> String -> IO ()
+action s msg inf | "HELO" `isInfixOf` msg   = void  $ send s (pack $ msg++ inf)
+                 | "KILL_SERVICE\n" == msg  = exitSuccess
+                 | otherwise                = return ()
 
+{- Preforms an operation on a the contents of a mutex variable while maintaining atomicity -}
+updateMutex :: MVar a -> (a -> a) -> IO ()
+updateMutex mv op = do
+  x <- takeMVar mv
+  putMVar mv (op x)
 
-action :: Net.Socket -> String -> [String] -> IO ()
-action s msg inf | "HELO" `isInfixOf` msg = void  $ send s (pack $ getDetails msg inf)
-                 | "KILL_SERVICE\n" `isInfixOf` msg = exitSuccess
-                 | otherwise = void $  send s $ pack msg
-
-handle :: Net.Socket -> [String] -> IO ()
+{- Handle an incoming connection, ideally should be run on a new thread to allow multiple
+  simultaneous server connections -}
+handle :: Net.Socket -> String -> IO ()
 handle sock inf = do
-    msg <- Network.Socket.ByteString.recv sock 4096
-    print $ "Message = " ++ unpack msg
-    unless (B.null msg) $ action sock (unpack msg) inf >> handle sock inf
+  msg <- readAll sock 1 (return []) -- read the entire message from the socket
+  print $ "Message = " ++ unpack msg
+  unless (B.null msg) $ action sock (unpack msg) inf >> handle sock inf
+  print "Im back yo"
 
-runServer :: Net.Socket -> Int -> [String]-> IO ()
+{- reads the entire message from the socket, this is used instead of default recv function
+   as recv is not guaranteed to return the entire message -}
+readAll :: Net.Socket -> Int -> IO [B.ByteString] -> IO B.ByteString 
+readAll _ 0 iob = fmap B.concat iob
+readAll s _ m = do
+  msg <- recv s 4096
+  readAll s (B.length msg) $ m >>= (\p -> return(p++[msg]))
+
+{- Handles the termination of connection handling threads -}
+endThread :: Net.Socket -> MVar Int -> IO ()
+endThread s m = Net.close s >> updateMutex m (+1)
+
+{-
+ Main logic of the Server
+ @params:
+    Socket on which the server should accept connections
+    Number of connections it should procees at a time
+    list of details regarding the server -}
+runServer :: Net.Socket -> Int -> String-> IO ()
 runServer sock n inf = do
-    Net.listen sock n
-    (conn,_) <- Net.accept sock
-    handle conn inf
-    Net.close conn
+  Net.listen sock 3                    -- allow maximum of 3 queued connections
+  threads <- newMVar n
+  loop sock threads inf
+  where loop s mu i =  do
+          (conn,_) <- Net.accept sock  -- accept incoming connection
+          x <- takeMVar mu
+          putMVar mu x
+          if x == 0                    -- if no free threads available
+            then Net.close conn >> loop s mu i
+            else do
+            updateMutex mu (\z -> z-1) -- reduce available thread count
+            _ <- forkFinally (handle conn inf) (\_ -> endThread conn mu )
+            loop s mu i
 
 main :: IO ()
 main = do
-  (n:port:host:_) <- getArgs
-  sock <- initServer host port
-  runServer sock (read n :: Int) [host, port, "13319506"]
-  putStrLn "hello world"
+  [host, port, n] <- getArgs
+  sock <- initServer host port -- intialise the server socket
+  runServer sock  (read n :: Int) $ "IP:"++host++"\nPort:"++port++"\nSudentID:13319506\n"
+
